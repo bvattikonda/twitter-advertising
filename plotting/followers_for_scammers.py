41a688b96104fdfa4425a7f0135032de636d9a92
Bhanu Vattikonda <bvattikonda@cs.ucsd.edu>
2012-02-13 01:19:03 +0000

    Identify how many followers do the scammers have

---
+++ plotting/followers_for_scammers.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Script to plot the CDF of amount of money that the publishers make on adly
+
+import sys
+import datetime
+import string
+import argparse
+import os
+import json
+import ast
+
+def get_args():
+    parser = argparse.ArgumentParser(description = 'Generate earnings graph')
+    parser.add_argument('--extension', default = '.png',\
+        help = 'File format in which graphs are wanted (eps or png)')
+    parser.add_argument('--users_dir', required = True,\
+        help = 'Directory which has user data')
+    parser.add_argument('--links_dir', required = True,\
+        help = 'Directory which has links data')
+    return parser.parse_args()
+
+def get_follower_count(users_dir, user_id):
+    userfile = open(os.path.join(users_dir, str(user_id) + '.txt'), 'r')
+    userfile.readline()
+    followers = json.loads(userfile.readline())
+    print len(followers)
+    return len(followers)
+    
+def get_plot_data(users_dir, links_dir):
+    plot_data = list()
+    linksfilenames = os.listdir(links_dir)
+    for linksfilename in linksfilenames:
+        linksfile = open(os.path.join(links_dir, linksfilename), 'r')
+        for line in linksfile:
+            if 'clickbank.net' in line:
+                follower_count = get_follower_count(users_dir,
+                    int(linksfilename.replace('.links', '')))
+                plot_data.append(follower_count)
+                break
+    return plot_data
+
+def main():
+    args = get_args()    
+    plot_data = get_plot_data(args.users_dir, args.links_dir)
+    print len(plot_data)
+    f = open('temp.txt', 'w')
+    for item in plot_data:
+        print >>f, item
+
+if __name__ == '__main__':
+    main()
