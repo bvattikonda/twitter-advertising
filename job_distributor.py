c695844c53e605093173bea37900db1d6fce13ae
Bhanu Vattikonda <vbhanu@gmail.com>
2012-03-23 15:29:39 -0400

    Job distributor and receiver

--- job_distributor.py
+++ job_distributor.py
@@ -1,18 +1,42 @@
 from twisted.internet.protocol import ServerFactory
 from twisted.internet.protocol import Protocol
 import logging
+import os
+import re
 import time
 
 PORT = 9000
+
 class JobDistributor(Protocol):
     def dataReceived(self, data):
-        splitdata = data.split('\t')
-        print splitdata
-        self.transport.write('This is the new data')
+        path, pattern = data.split('\t')
+        if (path, pattern) not in self.factory.job_listings:
+            self.factory.job_listings[(path, pattern)] = list()
+            self.factory.finished_jobs[(path, pattern)] = list()
+
+        if not self.factory.job_listings:
+            filenames = os.listdir(path)
+            for filename in filenames:
+                if re.match(pattern, filename) and filename not in\
+                    self.factory.finished_jobs:
+                    self.factory.job_listings[(path,\
+                            pattern)].append(filename)
+
+        if len(self.factory.job_listings) == 0:
+            self.transport.write('')
+        else:
+            job = self.factory.job_listings[0]
+            self.transport.write(job)
+            self.factory.finished_jobs.append(job)
+            del self.factory.job_listings[0]
         self.transport.loseConnection()
 
 class JobDistributionFactory(ServerFactory):
     protocol = JobDistributor
+
+    def __init__(self):
+        self.job_listings = dict()
+        self.finished_jobs = dict()
 
 def main():
     logging.basicConfig(filename = 'job_distributor.log',
--- streaming_data_redirects.py
+++ streaming_data_redirects.py
@@ -16,6 +16,21 @@ import inspect
 from threading import Thread
 import threading
 
+def get_job(server, port, path, pattern):
+    tn = Telnet()
+    tn.open(server, port)
+    tn.write(path + '\t' + pattern)
+    return tn.read_all()
 
+def main():
+    f = open('temp.txt', 'w')
+    while True:
+        job = get_job('sysnet82.sysnet.ucsd.edu', 9000,\
+                '/mnt/bvattikonda/stream_data',\
+                'links_*.links')
+        time.sleep(2)
+        print job 
+        print >>f, job
+    
 if __name__ == '__main__':
     main()
